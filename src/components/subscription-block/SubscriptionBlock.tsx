3154553b1eec37ec3e8da664266cb0b3fde50c8f
EugeneKh1988 <khiznyak.1988@gmail.com>
2022-07-13 18:22:40 +0300

    Added validation

--- src/components/subscription-block/SubscriptionBlock.tsx
+++ src/components/subscription-block/SubscriptionBlock.tsx
@@ -1,4 +1,5 @@
 import { SubscriptionBlockSvgSelector } from './SubscriptionBlockSvgSelector';
+import { Formik, Field, Form } from 'formik';
 
 export const SubscriptionBlock: React.FC = () => {
   return (
@@ -17,21 +18,58 @@ export const SubscriptionBlock: React.FC = () => {
           <SubscriptionBlockSvgSelector id='logo' />
         </div>
         <div className='subscription-block__form-part'>
-          <div className='subscription-block__form-fields'>
-            <input
-              type='tel'
-              className='subscription-block__phone-field'
-              placeholder='Номер телeфону'
-            />
-            <input
-              type='email'
-              className='subscription-block__email-field'
-              placeholder='Email'
-            />
-            <button type='submit'>
-              <span>Підписатися</span>
-            </button>
-          </div>
+          <Formik
+            initialValues={{ tel: '', email: '' }}
+            validate={values => {
+              const errors: { tel?: string, email?: string } = {};
+              if (!values.tel) {
+                errors.tel = "Телефон є обов'язковим";
+              }
+              else if (!/^\+38[0-9]{3}[0-9]{2}[0-9]{2}[0-9]{3}$/i.test(values.tel)) {
+                errors.tel = "Невірний номер телефону";
+              }
+              if (!values.email) {
+                errors.tel = "Телефон є обов'язковим";
+              }
+              else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = "Неправильна ел. адреса";
+              }
+              return errors;
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              setSubmitting(false);
+            }}
+          >
+            {({ values, errors, touched }) => (
+              <Form>
+                <div className='subscription-block__form-fields'>
+                  <div>
+                    <Field
+                      type='tel'
+                      className='subscription-block__phone-field'
+                      placeholder='Номер телeфону'
+                      name="tel"
+                      id="tel"
+                    />
+                    {touched.tel && errors.tel ? <div className="login-block-input__error">{errors.tel}</div> : null}
+                  </div>
+                  <div>
+                    <Field
+                      type='email'
+                      className='subscription-block__email-field'
+                      placeholder='Email'
+                      name="email"
+                      id="email"
+                    />
+                    {touched.email && errors.email ? <div className="login-block-input__error">{errors.email}</div> : null}
+                  </div>
+                  <button type='submit'>
+                    <span>Підписатися</span>
+                  </button>
+                </div>
+              </Form>
+            )}
+          </Formik>
           <a
             className='subscription-block__confidential-policy-href'
             href='https://www.youtube.com/channel/UCVlYt9-6GLSwysPNCJ7C8lw'
